b4f4fcabfcb8ff70402848579a7367bca523bd01
Jerry Vigil <vigilj@google.com>
2015-03-27 17:22:20 -0700

    9

--- safeway.py
+++ safeway.py
@@ -1,10 +1,12 @@
+#import time 
+
 from selenium import webdriver
 from selenium.webdriver.support import expected_conditions
 
 def main():
+  USER_ID = ''
   CONTRASENA = ''
-  USER_ID = ''	
-
+  
   driver = webdriver.Firefox()
   driver.implicitly_wait(8)
   driver.get('https://www.safeway.com/ShopStores/OSSO-Login.page')
@@ -17,8 +19,12 @@ def main():
 
   # Sleep and then proceed with the checks.
   # Do another wait here.
-
+  j4u_link = driver.find_element_by_xpath('//a[@href="/ShopStores/Offers-Landing-IMG.page"]')
   driver.get('http://www.safeway.com/ShopStores/Justforu-PersonalizedDeals.page')
+  disclaimer_div = driver.find_element_by_id('disclaimer-1007')
+  driver.get('http://www.safeway.com/ShopStores/Justforu-CouponCenter.page')
+  disclaimer_div_ii = driver.find_element_by_id('disclaimer-1007')
+
   driver.quit()
 
 main()
