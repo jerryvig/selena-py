c3b9c15962d2e80975ac1b7901c8e90b3b8c62b6
Jerry Vigil <vigilj@google.com>
2017-02-17 12:58:07 -0800

    Adding morningstar_selena which uses chrome driver at this point.

---
+++ morningstar_selena.py
@@ -0,0 +1,32 @@
+from pyglib import app
+from selenium import webdriver
+
+
+def main(unused_argv):
+  ticker_list = ['AAPL', 'GOOGL', 'MSFT', 'FB', 'AMZN']
+
+  try:
+    browser = webdriver.Chrome()
+    browser.get('http://financials.morningstar.com/income-statement/is.html?t=%s&region=USA' % ticker_list[3])
+    body_element = browser.find_element_by_tag_name('body')
+    revenue_row_elements = body_element.find_elements_by_xpath('//div[@id="data_i1"]')
+    if revenue_row_elements:
+      revenue_row = revenue_row_elements[0]
+      y1_element = revenue_row.find_element_by_id('Y_1')
+      y2_element = revenue_row.find_element_by_id('Y_2')
+      y3_element = revenue_row.find_element_by_id('Y_3')
+      y4_element = revenue_row.find_element_by_id('Y_4')
+      if y1_element:
+        print 'year one raw value = ' + str(y1_element.get_attribute('rawvalue'))
+      if y2_element:
+        print 'year two raw value = ' + str(y2_element.get_attribute('rawvalue'))
+      if y3_element:
+        print 'year three raw val = ' + str(y3_element.get_attribute('rawvalue'))
+      if y4_element:
+        print 'year four raw val = ' + str(y4_element.get_attribute('rawvalue'))
+  finally:
+    browser.close()
+    quit()
+
+if __name__ == '__main__':
+  app.run()
