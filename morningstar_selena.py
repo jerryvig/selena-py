34264dddfa5e0df2dadb840b842de1fac64ada4d
Jerry Vigil <vigilj@google.com>
2017-02-17 13:08:02 -0800

    a

--- morningstar_selena.py
+++ morningstar_selena.py
@@ -2,11 +2,20 @@ from pyglib import app
 from selenium import webdriver
 
 
+def get_browser(name):
+  if name == 'Chrome':
+    return webdriver.Chrome()
+  elif name == 'Firefox':
+    return webdriver.Firefox()
+  elif name == 'PhantomJS':
+    return webdriver.PhantomJS()
+
+
 def main(unused_argv):
   ticker_list = ['AAPL', 'GOOGL', 'MSFT', 'FB', 'AMZN']
 
   try:
-    browser = webdriver.Chrome()
+    browser = get_browser('Chrome')
     browser.get('http://financials.morningstar.com/income-statement/is.html?t=%s&region=USA' % ticker_list[3])
     body_element = browser.find_element_by_tag_name('body')
     revenue_row_elements = body_element.find_elements_by_xpath('//div[@id="data_i1"]')
