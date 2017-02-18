63e05af8a1f954dad8f39e488c05be574db00c7b
Jerry Vigil <vigilj@google.com>
2017-02-17 19:12:18 -0800

    more edits

--- safeway.py
+++ safeway.py
@@ -4,7 +4,8 @@ from selenium import webdriver
 
 USER_ID = 'agentq314@yahoo.com'
 CONTRASENA = 'overthere'
-
+LOGIN_PAGE_URL = 'https://www.safeway.com/CMS/account/login/'
+PERSONALIZED_DEALS_PAGE_URL = 'http://www.safeway.com/ShopStores/Justforu-Coupons.page#/offerTypes/PD'
 
 def get_driver(name):
   if name == 'Chrome':
@@ -16,8 +17,7 @@ def get_driver(name):
 
 
 def load_personalized(driver):
-  driver.get(
-      'http://www.safeway.com/ShopStores/Justforu-Coupons.page#/offerTypes/PD')
+  driver.get(PERSONALIZED_DEALS_PAGE_URL)
   elements = driver.find_element_by_xpath('//a[@href="#/offerTypes/PD"]')
   log.info('elements = %s', elements)
   # Continue the logic here...
@@ -27,15 +27,15 @@ def main(unused_argv):
   driver = get_driver('Chrome')
   try:
     driver.implicitly_wait(8)
-    driver.get('https://www.safeway.com/CMS/account/login/')
+    driver.get(LOGIN_PAGE_URL)
     user_id_input = driver.find_element_by_id('input-email')
     user_id_input.send_keys(USER_ID)
     contrasena = driver.find_element_by_id('password-password')
     contrasena.send_keys(CONTRASENA)
-    sign_in = driver.find_element_by_id('create-account-btn')
-    sign_in.send_keys('\n')
-    driver.implicitly_wait(5)
-
+    sign_in_boton = driver.find_element_by_id('create-account-btn')
+    sign_in_boton.send_keys('\n')
+    sign_in_boton.click()
+    
     load_personalized(driver)
   finally:
     driver.close()
