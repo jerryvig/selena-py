03a2812474d63e7f2f2f394cdf49c9fe9a10bec7
Jerry Vigil <vigilj@google.com>
2017-02-17 18:39:47 -0800

    fixing some lint errors.

--- safeway.py
+++ safeway.py
@@ -1,7 +1,9 @@
 from pyglib import app
 from pyglib import log
 from selenium import webdriver
-from selenium.webdriver.support import expected_conditions
+
+USER_ID = 'agentq314@yahoo.com'
+CONTRASENA = 'overthere'
 
 
 def get_driver(name):
@@ -13,38 +15,28 @@ def get_driver(name):
     return webdriver.PhantomJS()
 
 
-def loadPersonalizedDeals(driver):
-  driver.get('http://www.safeway.com/ShopStores/Justforu-Coupons.page#/offerTypes/PD')
-  try:
-    elements = driver.find_element_by_xpath('//a[@href="#/offerTypes/PD"]')
-    log.info('elements = %s', elements) 
-    # Continue the logic here.
-  except:
-    log.info('GIVING UP')
+def load_personalized(driver):
+  driver.get(
+      'http://www.safeway.com/ShopStores/Justforu-Coupons.page#/offerTypes/PD')
+  elements = driver.find_element_by_xpath('//a[@href="#/offerTypes/PD"]')
+  log.info('elements = %s', elements)
+  # Continue the logic here...
 
 
 def main(unused_argv):
-  USER_ID = 'agentq314@yahoo.com'
-  CONTRASENA = 'overthere'
-  
   driver = get_driver('Chrome')
   try:
     driver.implicitly_wait(8)
     driver.get('https://www.safeway.com/CMS/account/login/')
-    userIdInput = driver.find_element_by_id('input-email')
-    userIdInput.send_keys(USER_ID)
+    user_id_input = driver.find_element_by_id('input-email')
+    user_id_input.send_keys(USER_ID)
     contrasena = driver.find_element_by_id('password-password')
     contrasena.send_keys(CONTRASENA)
-    signIn = driver.find_element_by_id('create-account-btn')
-    signIn.send_keys('\n')
+    sign_in = driver.find_element_by_id('create-account-btn')
+    sign_in.send_keys('\n')
+    driver.implicitly_wait(5)
 
-    # Sleep and then proceed with the checks.
-    # Do another wait here.
-    try:
-      j4u_link = driver.find_element_by_xpath('//a[@href="/ShopStores/Offers-Landing-IMG.page"]')
-      loadPersonalizedDeals(driver)
-    except:
-      loadPersonalizedDeals(driver)
+    load_personalized(driver)
   finally:
     driver.close()
 
