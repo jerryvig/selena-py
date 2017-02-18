115a2ee7c05eec5d40e1752cbfc9d010d5112c67
Jerry Vigil <vigilj@google.com>
2017-02-17 18:02:51 -0800

    more updates

--- safeway.py
+++ safeway.py
@@ -1,38 +1,54 @@
 from pyglib import app
+from pyglib import log
 from selenium import webdriver
 from selenium.webdriver.support import expected_conditions
+
+
+def get_driver(name):
+  if name == 'Chrome':
+    return webdriver.Chrome()
+  elif name == 'Firefox':
+    return webdriver.Firefox()
+  elif name == 'PhantomJS':
+    return webdriver.PhantomJS()
+
 
 def loadPersonalizedDeals(driver):
   driver.get('http://www.safeway.com/ShopStores/Justforu-Coupons.page#/offerTypes/PD')
   try:
-    driver.find_element_by_xpath('//a[@href="#/offerTypes/PD"]')
+    elements = driver.find_element_by_xpath('//a[@href="#/offerTypes/PD"]')
+    log.info('elements = %s', elements) 
     # Continue the logic here.
-    driver.quit()
   except:
-    print 'GIVING UP'
-    driver.quit()
+    log.info('GIVING UP')
+
 
 def main(unused_argv):
   USER_ID = 'agentq314@yahoo.com'
   CONTRASENA = 'overthere'
   
-  driver = webdriver.Firefox()
-  driver.implicitly_wait(8)
-  driver.get('https://www.safeway.com/ShopStores/OSSO-Login.page')
-  userIdInput = driver.find_element_by_id('userId')
-  userIdInput.send_keys(USER_ID)
-  contrasena = driver.find_element_by_id('password')
-  contrasena.send_keys(CONTRASENA)
-  signIn = driver.find_element_by_id('SignInBtn')
-  signIn.send_keys('\n')
-
-  # Sleep and then proceed with the checks.
-  # Do another wait here.
+  driver = get_driver('Chrome')
   try:
-    j4u_link = driver.find_element_by_xpath('//a[@href="/ShopStores/Offers-Landing-IMG.page"]')
-    loadPersonalizedDeals(driver)
-  except:
-    loadPersonalizedDeals(driver)
+    driver.implicitly_wait(8)
+    driver.get('https://www.safeway.com/CMS/account/login/')
+    userIdInput = driver.find_element_by_id('input-email')
+    userIdInput.send_keys(USER_ID)
+    contrasena = driver.find_element_by_id('password-password')
+    contrasena.send_keys(CONTRASENA)
+    signIn = driver.find_element_by_id('create-account-btn')
+    signIn.send_keys('\n')
+
+    # Sleep and then proceed with the checks.
+    # Do another wait here.
+    try:
+      j4u_link = driver.find_element_by_xpath('//a[@href="/ShopStores/Offers-Landing-IMG.page"]')
+      loadPersonalizedDeals(driver)
+    except:
+      loadPersonalizedDeals(driver)
+  finally:
+    driver.close()
+
 
 if __name__ == '__main__':
   app.run()
+
