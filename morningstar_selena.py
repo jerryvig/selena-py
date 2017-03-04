8607761185afca5b3170a4dc319c072e16fcb20c
Jerry Vigil <vigilj@google.com>
2017-03-03 20:00:42 -0800

    a

--- morningstar_selena.py
+++ morningstar_selena.py
@@ -1,3 +1,4 @@
+import time
 from pyglib import app
 from pyglib import gflags
 from selenium import webdriver
@@ -25,7 +26,7 @@ def get_browser(name):
 
 
 def main(unused_argv):
-  ticker_list = ['AAPL', 'GOOGL', 'MSFT', 'FB', 'AMZN', 'NFLX', 'TSLA']
+  tickers = ('AAPL', 'GOOGL', 'MSFT', 'FB', 'AMZN', 'NFLX', 'TSLA', 'TWTR', 'BABA')
 
   browser = get_browser(FLAGS.browser_driver)
   if not browser:
@@ -33,29 +34,32 @@ def main(unused_argv):
         'Failed to create browser instance with flag %s' % FLAGS.browser_driver)
 
   try:
-    browser.get('http://financials.morningstar.com/income-statement/is.html?t=%s&region=USA' % ticker_list[3])
-    body_element = browser.find_element_by_tag_name('body')
-    revenue_row_elements = body_element.find_elements_by_xpath('//div[@id="data_i1"]')
-    if revenue_row_elements:
-      revenue_row = revenue_row_elements[0]
-      y1_element = revenue_row.find_element_by_id('Y_1')
-      y2_element = revenue_row.find_element_by_id('Y_2')
-      y3_element = revenue_row.find_element_by_id('Y_3')
-      y4_element = revenue_row.find_element_by_id('Y_4')
-      y5_element = revenue_row.find_element_by_id('Y_5')
-      y6_element = revenue_row.find_element_by_id('Y_6')
-      if y1_element:
-        print 'year one raw value = ' + str(y1_element.get_attribute('rawvalue'))
-      if y2_element:
-        print 'year two raw value = ' + str(y2_element.get_attribute('rawvalue'))
-      if y3_element:
-        print 'year three raw val = ' + str(y3_element.get_attribute('rawvalue'))
-      if y4_element:
-        print 'year four raw val = ' + str(y4_element.get_attribute('rawvalue'))
-      if y5_element:
-        print 'year five raw val = ' + str(y5_element.get_attribute('rawvalue'))
-      if y6_element:
-        print 'year six raw val = ' + str(y6_element.get_attribute('rawvalue'))
+    for symbol in tickers:
+      browser.get('http://financials.morningstar.com/income-statement/is.html?t=%s&region=USA' % symbol)
+      body_element = browser.find_element_by_tag_name('body')
+      revenue_row_elements = body_element.find_elements_by_xpath('//div[@id="data_i1"]')
+      if revenue_row_elements:
+        revenue_row = revenue_row_elements[0]
+        y1_element = revenue_row.find_element_by_id('Y_1')
+        y2_element = revenue_row.find_element_by_id('Y_2')
+        y3_element = revenue_row.find_element_by_id('Y_3')
+        y4_element = revenue_row.find_element_by_id('Y_4')
+        y5_element = revenue_row.find_element_by_id('Y_5')
+        y6_element = revenue_row.find_element_by_id('Y_6')
+        if y1_element:
+          print 'year one raw value = ' + str(y1_element.get_attribute('rawvalue'))
+        if y2_element:
+          print 'year two raw value = ' + str(y2_element.get_attribute('rawvalue'))
+        if y3_element:
+          print 'year three raw val = ' + str(y3_element.get_attribute('rawvalue'))
+        if y4_element:
+          print 'year four raw val = ' + str(y4_element.get_attribute('rawvalue'))
+        if y5_element:
+          print 'year five raw val = ' + str(y5_element.get_attribute('rawvalue'))
+        if y6_element:
+          print 'year six raw val = ' + str(y6_element.get_attribute('rawvalue'))
+
+      time.sleep(1)
   finally:
     browser.close()
     quit()
